a5320a56f6e3b5a9b57db6398678f555a9a670a0
Aqil321821 <aqilrao321821@gmail.com>
2025-11-25 14:27:13 +0500

    deleted buisness website

--- 09-Buisness-website/script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.menu')
-const navbar = document.querySelector('.navbar')
-
-menu.addEventListener('click', () => {
-    navbar.classList.toggle('active')
-    menu.classList.toggle('active')
-})
