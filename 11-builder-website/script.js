49978fe708f3ac02ab97b2d4540fc9c0614abe7a
Aqil321821 <aqilrao321821@gmail.com>
2025-12-02 18:37:33 +0500

    sidebar done

---
+++ 11-builder-website/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+  document.querySelector('.sidebar').classList.toggle('active');
+  document.querySelector('.hamburger-menu').classList.toggle('active');
+});
