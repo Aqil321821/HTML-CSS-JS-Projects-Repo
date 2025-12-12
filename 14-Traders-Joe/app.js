7bd52e2b08a3ce4fba1f2d9aaaea0d1c484fd1c9
Aqil321821 <aqilrao321821@gmail.com>
2025-12-12 18:27:46 +0500

    added 4 projects from other repo

---
+++ 14-Traders-Joe/app.js
@@ -0,0 +1,83 @@
+let hamBtn = document.getElementById('ham-btn-desk');
+let crossBtn = document.getElementById('corss-btn-desk');
+
+let hamBtnMob = document.getElementById('ham-btn-mob');
+let crossBtnMob = document.getElementById('corss-btn-mob');
+let mobOffCanvas = document.getElementById('off-mobile');
+
+let offCanvas = document.getElementById('my-menu-offcanvas1');
+let myNav = document.getElementById('myNav');
+let bookmark = document.getElementById('bookmark');
+
+hamBtn.addEventListener('click', () => {
+  hamBtn.classList.add('d-none');
+  crossBtn.classList.remove('d-none');
+  offCanvas.classList.add('active');
+  myNav.classList.add('bg-nav-cream');
+  document.body.classList.add('menu-open');
+  window.scrollTo(0, 0);
+});
+crossBtn.addEventListener('click', () => {
+  crossBtn.classList.add('d-none');
+  hamBtn.classList.remove('d-none');
+  offCanvas.classList.remove('active');
+  myNav.classList.remove('bg-nav-cream');
+  document.body.classList.remove('menu-open');
+});
+hamBtnMob.addEventListener('click', () => {
+  hamBtnMob.classList.add('d-none');
+  crossBtnMob.classList.remove('d-none');
+  mobOffCanvas.classList.add('active');
+  myNav.classList.add('bg-nav-cream');
+  document.body.classList.add('menu-open');
+  window.scrollTo(0, 0);
+});
+crossBtnMob.addEventListener('click', () => {
+  crossBtnMob.classList.add('d-none');
+  hamBtnMob.classList.remove('d-none');
+  mobOffCanvas.classList.remove('active');
+  myNav.classList.remove('bg-nav-cream');
+  document.body.classList.remove('menu-open');
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    bookmark.classList.remove('one');
+    bookmark.classList.add('zero');
+  } else if (window.scrollY === 0) {
+    bookmark.classList.add('one');
+    bookmark.classList.remove('zero');
+  }
+});
+
+window.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+});
+
+// window.addEventListener('resize', setBookmarkTop);
+// window.addEventListener('scroll', setBookmarkTop);
+
+// function setBookmarkTop() {
+//   const navbar = document.getElementById('myNav');
+//   const bookmark = document.getElementById('bookmark');
+//   if (navbar && bookmark) {
+//     const navHeight = navbar.offsetHeight;
+//     bookmark.style.top = 50 + navHeight + 'px';
+//   }
+// }
+// setBookmarkTop();
+
+// swiper
+new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
---
+++ 15-Lego/script.js
@@ -0,0 +1,180 @@
+// document.getElementById('toggle-debug').addEventListener('click', function () {
+//   document.body.classList.toggle('debug');
+// });
+
+new Swiper('.mySwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+new Swiper('#slider2', {
+  slidesPerView: 'auto', // slides ki width CSS se aayegi
+  spaceBetween: 20, // slides ke beech ka gap
+  freeMode: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+    hide: false,
+    snapOnRelease: false,
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    992: { slidesPerView: 3 },
+  },
+});
+
+new Swiper('#slider3', {
+  slidesPerView: 'auto', // slides ki width CSS se aayegi
+  spaceBetween: 20, // slides ke beech ka gap
+  freeMode: true,
+  scrollbar: {
+    el: '.swiper-scrollbar-3',
+    draggable: true,
+    hide: false,
+    snapOnRelease: false,
+  },
+});
+new Swiper('#slider4', {
+  slidesPerView: 'auto', // slides ki width CSS se aayegi
+  spaceBetween: 20, // slides ke beech ka gap
+  freeMode: true,
+  scrollbar: {
+    el: '.swiper-scrollbar-4',
+    draggable: true,
+    hide: false,
+    snapOnRelease: false,
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    992: { slidesPerView: 3 },
+  },
+});
+
+const swiper5 = new Swiper('#slider5', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+
+  // scrollbar
+  scrollbar: {
+    el: '.bar-5',
+    draggable: true,
+  },
+
+  // navigation
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // responsive breakpoints
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
+// state to rep hide/show state of input bar
+let inpSearch = false;
+
+function openSearch() {
+  if (!inpSearch) {
+    document.getElementById('btnSearch').classList.add('d-none');
+    document.getElementById('inpSearch').classList.remove('d-none');
+    document.getElementById('box-shadow').innerHTML =
+      '<div onclick="openSearch()"  class="modal-backdrop fade show"></div>';
+    inpSearch = true;
+  } else {
+    document.getElementById('btnSearch').classList.remove('d-none');
+    document.getElementById('inpSearch').classList.add('d-none');
+    document.getElementById('box-shadow').innerHTML = '';
+    inpSearch = false;
+  }
+}
+
+// for res
+let inpSearchRS = false;
+
+function openSearchRS() {
+  if (!inpSearchRS) {
+    document.getElementById('btnSearchRS').classList.add('d-none');
+    document.getElementById('inpSearchRS').classList.remove('d-none');
+    document.getElementById('box-shadow').innerHTML =
+      '<div onclick="openSearch()"  class="modal-backdrop fade show"></div>';
+    inpSearchRS = true;
+  } else {
+    document.getElementById('btnSearchRS').classList.remove('d-none');
+    document.getElementById('inpSearchRS').classList.add('d-none');
+    document.getElementById('box-shadow').innerHTML = '';
+    inpSearchRS = false;
+  }
+}
+
+const searchInput = document.getElementById('searchInput');
+const searchIcon = document.getElementById('searchIcon');
+const clearBtn = document.getElementById('clearBtn');
+
+// when user starts to write than hide search icon
+searchInput.addEventListener('input', () => {
+  if (searchInput.value.trim() !== '') {
+    searchIcon.classList.add('d-none');
+  } else {
+    searchIcon.classList.remove('d-none');
+  }
+});
+//when user clicks on X then empty the field
+clearBtn.addEventListener('click', () => {
+  searchInput.value = '';
+  searchIcon.classList.remove('d-none');
+});
+
+// function to hide toppest div
+function closeTopbar() {
+  let topDiv = document.getElementById('top-div');
+  if (!topDiv.classList.contains('d-none')) {
+    topDiv.classList.add('d-none');
+  }
+}
+
+window.addEventListener('resize', () => {
+  let topDiv = document.getElementById('top-div');
+  if (topDiv.classList.contains('d-none')) {
+    if (window.innerWidth >= 991) {
+      topDiv.classList.remove('d-none');
+      // alert('hello');
+    }
+  }
+});
+
+//function to animate bars when btn is clicked
+function animateBars() {
+  let bar_1 = document.querySelector('.icon-bar-1');
+  let bar_2 = document.querySelector('.icon-bar-2');
+  let bar_3 = document.querySelector('.icon-bar-3');
+
+  bar_1.classList.toggle('icon-bar-1-active');
+  bar_2.classList.toggle('icon-bar-2-active');
+  bar_3.classList.toggle('icon-bar-3-active');
+}
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// scroll reset:
+// Save scroll position
+// window.addEventListener('beforeunload', () => {
+//   localStorage.setItem('scrollPos', window.scrollY);
+// });
+
+// Restore scroll position
+// window.addEventListener('load', () => {
+//   const scrollPos = localStorage.getItem('scrollPos');
+//   if (scrollPos) {
+//     window.scrollTo(0, parseInt(scrollPos));
+//     localStorage.removeItem('scrollPos');
+//   }
+// });
